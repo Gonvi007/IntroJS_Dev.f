a4f37305cd985caf571d133454c5666be546ef1b
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-06-30 07:56:50 -0500

    checando las arrow function

--- 01 Intro Js/Array/main.js
+++ 01 Intro Js/Array/main.js
@@ -4,7 +4,7 @@
 var verduras = ['Cebolla', 'Perejil', 'Tomate', 'Calabaza', 'Brocoli'];
 verduras.sort();
 
-// Para ordenar numero no funciona de la misma manera, se debe hacer ed esta manera...
+// Para ordenar numero no funciona de la misma manera, se debe hacer de esta manera...
 
 [2, 5, 1, 3, 45, 70, 34].sort(function(a, b){return a-b});
 // Y daria el orden correcto de los numeros
@@ -33,5 +33,46 @@ var i = 0;
 while(i < 11) {
     console.log(i);
     i++;
-
 }
+
+
+
+// const saludo = () => console.log(`Hola`);
+
+// saludar();
+
+const saludo = nombre => console.log(`Hola ${nombre}`);
+
+saludar("Irma");
+
+// const sumar = function (a,b) {
+//     return a + b;
+// }
+
+const sumar = (a,b) => a + b;
+
+console.log(sumar(8, 9));
+
+const funcionDeVariasLienas = () => {
+    console.log("Uno");
+    console.log("Dos");
+    console.log("Tres");
+}
+funcionDeVariasLienas();
+
+const numeros = [1, 2, 3, 4, 5];
+
+numeros.forEach((el, index) => console.log(`${el} esta en la posición ${index}`));
+
+function perro(){
+    console.log(this);
+}
+perro();
+
+const perro = {
+    nombre: "ashjk",
+    ladrar(){
+        console.log(this)
+    }
+}
+perro.ladrar();
