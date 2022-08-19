090e755d39c9c04242e5adb356dc9dcc251b8624
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-08-19 09:09:56 -0500

    Clase de Platzi POO

--- CursoPOOUber/JavaScript/Index.js
+++ CursoPOOUber/JavaScript/Index.js
@@ -1,3 +1,10 @@
-let car = new Car("AW456", new Account("Andres Herrera", "asvhjk1"))
+var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
 car.passenger = 4;
-car.printDataCar();	
+car.printDataCar();
+
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA765"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
+
+
+
